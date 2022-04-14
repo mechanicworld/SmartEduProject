d164311247b0bcaea1a0f3509bac920a44c29f43
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2022-04-14 13:28:11 +0300

    1)Partials created 2)Index and About pages routing

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const ejs = require('ejs')
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"))
+
+// Routes
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World');
+  res.status(200).render('index', {
+    page_name:"index"
+  });
+});
+app.get('/about', (req, res) => {
+
+  res.status(200).render('about', {
+    page_name:"about"
+  });
 });
 
 const port = 3000;
